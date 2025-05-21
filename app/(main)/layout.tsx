5ca683971e5643cfb713a532322de35db92950fa
Jason Byun <jasonbyun5@gmail.com>
2025-05-21 10:55:43 -0400

    Finished the Sidebar

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import { Sidebar } from "@/components/sidebar";
+import { MobileHeader } from "@/components/mobile-header";
 
 type Props = {
     children: React.ReactNode;
@@ -7,9 +8,9 @@ type Props = {
 const MainLayout = ({ children, } : Props) =>{
     return(
         <>  
-            <ModileHeader />
+            <MobileHeader />
             <Sidebar className="hidden lg:flex"/>
-            <main className="lg:pl-[256px] h-full pt[50px] lg:pt-0">
+            <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
                 <div className="bg-red-500 h-full">
                     {children}
                 </div>
